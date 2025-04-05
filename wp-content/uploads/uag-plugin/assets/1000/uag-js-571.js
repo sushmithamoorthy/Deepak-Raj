94e786dd13dba12890e9a4d0963400fd5cfa8456
sushmitha <76438421+sushmithamoorthy@users.noreply.github.com>
2025-04-05 22:22:40 +0530

    changes

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-571.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('load', function() {
+        UAGBButtonChild.init('.uagb-block-85cf7f71');
+    });
+    window.addEventListener('load', function() {
+        UAGBCounter.init('.uagb-block-10979aef', {
+            "layout": "number",
+            "heading": "Years of Experience",
+            "numberPrefix": "",
+            "numberSuffix": "+",
+            "startNumber": 0,
+            "endNumber": 20,
+            "totalNumber": 100,
+            "decimalPlaces": 0,
+            "animationDuration": 1500,
+            "thousandSeparator": ",",
+            "circleSize": 230,
+            "circleStokeSize": 8,
+            "isFrontend": true
+        });
+    });
+    window.addEventListener('load', function() {
+        UAGBCounter.init('.uagb-block-793ba774', {
+            "layout": "number",
+            "heading": "Team Members",
+            "numberPrefix": "",
+            "numberSuffix": "+",
+            "startNumber": 0,
+            "endNumber": 15,
+            "totalNumber": 100,
+            "decimalPlaces": 0,
+            "animationDuration": 1500,
+            "thousandSeparator": ",",
+            "circleSize": 230,
+            "circleStokeSize": 8,
+            "isFrontend": true
+        });
+    });
+    window.addEventListener('load', function() {
+        UAGBButtonChild.init('.uagb-block-a14d830a');
+    });
+    window.addEventListener('load', function() {
+        UAGBButtonChild.init('.uagb-block-2d431a36');
+    });
+});
